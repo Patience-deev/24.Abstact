9699e98ed31ea1de8c162e1dc3da2505ce824c19
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-10 11:07:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstact
+{ abstract class vehicle
+    {
+        public int wheeels = 4;
+    }
+    class Corsa : vehicle 
+    {
+        public int sparewheels = 1;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Corsa car2 = new Corsa();
+            Console.WriteLine(car2.sparewheels);
+            Console.ReadLine();
+        }
+    }
+}
